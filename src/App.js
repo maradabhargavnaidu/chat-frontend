5ff9cac2def447b06ded2ba0aaa4e421c1aac72d
maradabhargavnaidu <bhargav.naidu.mbn.18@gmail.com>
2024-01-14 22:07:08 +0530

    full project and first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import soceketIOClient from "socket.io-client";
+import { Socket } from "socket.io-client";
+import React, { useState } from "react";
+import { useRef, useEffect } from "react";
+const socket = soceketIOClient.connect("http://localhost:3001");
 
 function App() {
+  const nameRef = useRef();
+  const roomRef = useRef();
+  const messageRef = useRef();
+  const [recdata, setRecdata] = useState([]);
+
+  function join() {
+    socket.emit("join_room", roomRef.current.value);
+  }
+  const sendmessage = () => {
+    socket.emit("send_message", {
+      roomid: roomRef.current.value,
+      username: nameRef.current.value,
+      message: messageRef.current.value,
+    });
+  };
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setRecdata((recdata) => [...recdata, data]);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <input type="text" placeholder="Username" ref={nameRef} /> */}
+      <input type="text" placeholder="Enter Room Number" ref={roomRef} />
+      <button onClick={() => join()}>Join</button>
+      <input type="text" placeholder="Enter Name" ref={nameRef} />
+      <input type="text" placeholder="Enter Message" ref={messageRef} />
+      <button onClick={() => sendmessage()}>send</button>
+      <h1>Messages</h1>
+      {recdata.map((data, index) => {
+        return (
+          <div>
+            <h1>{data.username}</h1>
+            <p>{data.message}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
